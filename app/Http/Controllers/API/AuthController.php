b3de7340671c3c814af558c5f9cbe0bd5319d779
Rafael <rafaelpereira_97@outlook.com>
2021-05-27 17:58:13 +0100

    Funcoes Login/Register

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Psy\Util\Str;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+
+        if($request->name && $request->email && $request->password != null){
+
+        $user =  User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'api_token' => Str::random(60),
+        ]);
+
+        return response()->json(['status' => 'success', 'api_token' => $user->api_token]);
+
+        }else{
+            return response()->json(['status' => 'error']);
+        }
+
+    }
+
+    public function login(Request $request){
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = \App\Models\User::where('email', $credentials['email'])->first();
+            $user->api_token = Str::random(60);
+            $user->save();
+
+            return response()
+                ->json(
+                    [
+                        'status' => 'success',
+                        'api_token' => $user->api_token
+                    ],200);
+        }
+
+        return response()
+            ->json(['status' => 'unauthorized'],401);
+
+    }
+}
